cc9247c785d2e49d1785c678a671956619909a76
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-24 15:42:14 -0500

    init

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ['<rootDir>/src'],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+};
