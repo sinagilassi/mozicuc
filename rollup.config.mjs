e15094a03a734284d0142a5d81220fc93e3388d1
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-24 16:40:09 -0500

    rollup init

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/index.cjs.js',
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: 'dist/index.esm.js',
+            format: 'esm',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+    ],
+    external: [], // Add external dependencies here if needed
+};
