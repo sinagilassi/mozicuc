eadae24ff668c3f2b7fd47be389d9026f8968fbd
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-24 16:46:34 -0500

    upd roll

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+// local
+const packageJson = require('./package.json');
 
 export default {
     input: 'src/index.ts',
     output: [
         {
-            file: 'dist/index.cjs.js',
+            file: packageJson.main,
             format: 'cjs',
             sourcemap: true,
         },
         {
-            file: 'dist/index.esm.js',
+            file: packageJson.module,
             format: 'esm',
             sourcemap: true,
         },
